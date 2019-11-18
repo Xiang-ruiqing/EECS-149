781c4b8945adaf3dbe825ecf52829900ff556646
Bryan Yang <bryang2199@gmail.com>
2019-11-18 15:11:03 -0800

    turn lights now blink
    added blinking to turn signals without using delay(), since delay() means the microcontroller can't read input during that time.

--- toggle_switch_test_149/toggle_switch_test_149.ino
+++ toggle_switch_test_149/toggle_switch_test_149.ino
@@ -1,11 +1,18 @@
+//states
 int left_state = 0; //variable for left toggle
 int left_state_prev = 0;
-int right_state = 0; //v-ariable for right toggle
+int right_state = 0; //variable for right toggle
 int right_state_prev = 0;
-int LEFT_SWITCH = 19;
-int RIGHT_SWITCH = 22;
-int LEFT_LED = 13;
-int RIGHT_LED = 12;
+//pins
+const int LEFT_SWITCH = 19;
+const int RIGHT_SWITCH = 22;
+const int LEFT_LED = 13;
+const int RIGHT_LED = 12;
+//timing/blinking stuff
+const long interval = 1000; //blink interval in milliseconds
+unsigned long prev_time = 0;
+int left_led_state = LOW;
+int right_led_state = LOW;
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,15 +26,23 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  unsigned long curr_time = millis();
+
   //left toggling
   if(digitalRead(LEFT_SWITCH) == HIGH) {
     left_state = 1 - left_state;
     Serial.print("Left state: ");
     Serial.println(left_state);
+    if (right_state == 1) {
+      right_state = 0;
+      right_led_state = LOW;
+      digitalWrite(RIGHT_LED, LOW);
+    }
   }
   //toggle on
   if(left_state == 1 && left_state_prev == 0) {
-    digitalWrite(LEFT_LED, HIGH);
+    //digitalWrite(LEFT_LED, HIGH);
+    //blink
     delay(500);
     Serial.println("left on");
   }
@@ -44,10 +59,16 @@ void loop() {
     right_state = 1 - right_state;
     Serial.print("Right state: ");
     Serial.println(right_state);
+    if (left_state == 1) {
+      left_state = 0;
+      left_led_state = LOW;
+      digitalWrite(LEFT_LED, LOW);
+    }
   }
   //toggle on
   if(right_state == 1 && right_state_prev == 0) {
-    digitalWrite(RIGHT_LED, HIGH);
+    //digitalWrite(RIGHT_LED, HIGH);
+    //blink
     delay(500);
     Serial.println("right on");
   }
@@ -58,4 +79,22 @@ void loop() {
     Serial.println("right off");
   }
   right_state_prev = right_state;
+
+  blink(LEFT_LED, left_state, &left_led_state, curr_time);
+  blink(RIGHT_LED, right_state, &right_led_state, curr_time);
+}
+
+void blink(int LED, int state, int* led_state, unsigned long curr_time) {
+  if (state == 1) {
+    if(curr_time - prev_time >= interval) {
+      prev_time = curr_time;
+      if (*led_state == HIGH) {
+        *led_state = LOW;
+        digitalWrite(LED, LOW);
+      } else {
+        *led_state = HIGH;
+        digitalWrite(LED, HIGH);
+      }
+    }
+  }
 }
