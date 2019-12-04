adfc563bc7eea16cdd61a8cdf488833fd82e8768
weiyuhe <39966010+weiyuhe@users.noreply.github.com>
2019-12-03 18:09:18 -0800

    Add files via upload

---
+++ Front_chip/Front_chip.ino
@@ -0,0 +1,292 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <MPU9250_asukiaaa.h>
+#ifdef _ESP32_HAL_I2C_H_
+#define SDA_PIN 21
+#define SCL_PIN 22
+#endif
+
+//switch pins
+const int LEFT_SWITCH = 34;
+const int RIGHT_SWITCH = 35;
+//const int LEFT_LED = 13;
+//const int RIGHT_LED = 12;
+
+const float LEFT_THRESH = 30.0;
+const float RIGHT_THRESH = -30.0;
+
+typedef enum {
+  OFF,
+  LEFT,
+  RIGHT,
+  PRE_LEFT,
+  PRE_RIGHT,
+} turn_state_t;
+
+typedef enum {
+  OFF_b,
+  ON,
+} brake_state_t;
+
+brake_state_t brake_state = OFF_b;
+turn_state_t turn_state = OFF;
+
+
+MPU9250_asukiaaa mySensor;
+float pitch, roll,yaw;
+float acc_x;
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 48;
+char buf[4];
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+
+void setup() {
+  while(!Serial);
+   
+  //pinMode(LEFT_SWITCH, INPUT);
+  //pinMode(RIGHT_SWITCH, INPUT);
+  Serial.begin(115200);
+  Serial.println("started");
+  
+  #ifdef _ESP32_HAL_I2C_H_
+  // for esp32
+  Wire.begin(SDA_PIN, SCL_PIN); //sda, scl
+  #else
+  Wire.begin();
+  #endif
+  
+  mySensor.setWire(&Wire);
+  
+  mySensor.beginAccel();
+  mySensor.beginMag();
+  mySensor.beginGyro();
+   // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+
+}
+
+bool just_left = false;
+bool just_right = false;
+char lights[3];
+
+void loop() {
+  //Serial.println(analogRead(RIGHT_SWITCH));
+  //delay(50);
+  mySensor.accelUpdate();
+  mySensor.magUpdate();
+  acc_x = mySensor.accelX();
+  char* acc_string = gcvt(acc_x,4,buf);
+  getAngle(mySensor.accelX(),mySensor.accelY(),mySensor.accelZ(),mySensor.magX(),mySensor.magY(),mySensor.magZ());
+  Serial.print("Pitch:\t"); 
+  Serial.println(pitch, 6);
+  Serial.print("Roll:\t"); 
+  Serial.println(roll, 6);
+  Serial.print("Yaw:\t"); 
+  Serial.println(yaw, 6);
+
+  switch(turn_state){
+    case OFF: {
+      Serial.println("OFF");
+      lights[0] = '0';
+      lights[2] = '0';
+      if (analogRead(LEFT_SWITCH) == 4095){
+        delay(500);
+        if (!just_left){
+          turn_state = PRE_LEFT;
+        }
+        just_left = true;
+        just_right = false;
+      }else if(analogRead(RIGHT_SWITCH) == 4095){
+        delay(500);
+        if (!just_right){
+          turn_state = PRE_RIGHT;
+        }
+        just_right = true;
+        just_left = false;
+      } else {
+        just_right = false;
+        just_left = false;
+      }    
+      break;
+      
+    }
+    case PRE_LEFT: {
+      Serial.println("PRE_LEFT");
+      lights[0] = '1';
+      lights[2] = '0';
+      if (analogRead(LEFT_SWITCH) == 4095){
+        delay(500);
+        if (!just_left){
+          turn_state = OFF;
+        }
+        just_left = true;
+        just_right = false;
+        
+      }else if(analogRead(RIGHT_SWITCH) == 4095){
+        delay(500);
+        if (!just_right){
+          turn_state = PRE_RIGHT;
+        }
+        just_left = false;
+        just_right = true;
+      } else if (roll >= LEFT_THRESH) {
+        //placeholder
+        turn_state = LEFT;
+        just_left = false;
+        just_right = false;
+      } else {
+        just_left = false;
+        just_right = false;
+      }
+      break;
+    }
+    case PRE_RIGHT: {
+      Serial.println("PRE_RIGHT");
+      lights[0] = '0';
+      lights[2] = '1';
+      if (analogRead(LEFT_SWITCH) == 4095){
+        delay(500);
+        if (!just_left){
+          turn_state = PRE_LEFT;
+        }
+        just_left = true;
+        just_right = false;
+      }else if(analogRead(RIGHT_SWITCH) == 4095){
+        delay(500);
+        if (!just_right){
+          turn_state = OFF;
+        }
+        just_left = false;
+        just_right = true;
+      } else if (roll <= RIGHT_THRESH) {
+        //placeholder
+        turn_state = RIGHT;
+        just_left = false;
+        just_right = false;
+      } else {
+        just_left = false;
+        just_right = false;
+      }
+      break;
+    }
+    case LEFT: {
+      Serial.println("LEFT");
+      lights[0] = '1';
+      lights[2] = '0'; 
+      if (roll < LEFT_THRESH / 2) {
+        turn_state = OFF;
+      }
+      just_left = false;
+      just_right = false;
+      break;
+    }
+    case RIGHT: {
+      Serial.println("RIGHT");
+      lights[0] = '0';
+      lights[2] = '1';
+      if (roll > RIGHT_THRESH / 2) {
+        turn_state = OFF;
+      }
+      just_left = false;
+      just_right = false;
+      break;
+    }
+    
+  };
+
+  switch (brake_state){
+    case OFF_b: {
+      lights[1] = '0';
+      if(acc_x <= 0){ // || vel == approx 0 (really need velocity?)
+        brake_state = ON;
+      }
+      break;
+    }
+    case ON: {
+      lights[1] = '1';
+      if(acc_x > 0){
+        brake_state = OFF_b;
+      }
+      break;
+    }
+  };
+  if (deviceConnected) {
+    pCharacteristic->setValue(lights);
+    pCharacteristic->notify();
+    delay(30); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+  }
+ 
+
+}
+
+void getAngle(float Vx, float Vy, float Vz, float magX, float magY, float magZ) {
+  float x = Vx;
+  float y = Vy;
+  float z = Vz;
+  pitch = atan(x / sqrt((y * y) + (z * z)));
+  roll = atan(y / sqrt((x * x) + (z * z)));
+
+  float Yh = (magY * cos(roll)) - (magZ * sin(roll));
+  float Xh = (magX * cos(pitch))+(magY * sin(roll)*sin(pitch)) + (magZ * cos(roll) * sin(pitch));
+
+   yaw =  atan2(-Yh, Xh);
+
+  pitch = pitch * (180.0 / 3.14);
+  roll = roll * (180.0 / 3.14) ;
+  yaw = yaw * (180.0/3.14);
+}
+
---
+++ back_chip/back_chip.ino
@@ -0,0 +1,240 @@
+/**
+   A BLE client example that is rich in capabilities.
+   There is a lot new capabilities implemented.
+   author unknown
+   updated by chegewara
+*/
+
+#include "BLEDevice.h"
+//#include "BLEScan.h"
+
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+// The characteristic of the remote service we are interested in.
+static BLEUUID    charUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLEAdvertisedDevice* myDevice;
+
+//LED stuff
+const int LEFT_LED = 13;
+const int BRAKE_LED = 21;
+const int RIGHT_LED = 12;
+int left_led_state = LOW;
+int brake_led_state = LOW;
+int right_led_state = LOW;
+int left = 0;
+int right = 0;
+//timing stuff
+const long blink_int = 1000;
+unsigned long prev_time = 0;
+
+static void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {
+  Serial.print("Notify callback for characteristic ");
+  Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+  Serial.print(" of data length ");
+  Serial.println(length);
+  Serial.print("data: ");
+  Serial.println((char*)pData);
+
+  char left_state = pData[0];
+  char brake_state = pData[1];
+  char right_state = pData[2];
+
+  Serial.println(left_state);
+  Serial.println(brake_state);
+  Serial.println(right_state);
+
+  if (brake_state == '0') {
+    brake_led_state = LOW;
+  } else {
+    brake_led_state = HIGH;
+  }
+  digitalWrite(BRAKE_LED, brake_led_state);
+
+  if (left_state == '1') {
+    Serial.println("Left");
+    digitalWrite(RIGHT_LED, LOW);
+    //digitalWrite(LEFT_LED, HIGH);
+    left = 1;
+    right = 0;
+  } else if (right_state == '1') {
+    Serial.println("Right");
+    digitalWrite(LEFT_LED, LOW);
+    //digitalWrite(RIGHT_LED, HIGH);
+    left = 0;
+    right = 1;
+  } else {
+    digitalWrite(LEFT_LED, LOW);
+    digitalWrite(RIGHT_LED, LOW);
+    left = 0;
+    right = 0;
+  }
+}
+
+void blink(int LED, int state, int* led_state, unsigned long curr_time) {
+  if (state == 1) {
+    if(curr_time - prev_time >= blink_int) {
+      prev_time = curr_time;
+      if (*led_state == HIGH) {
+        *led_state = LOW;
+        digitalWrite(LED, LOW);
+      } else {
+        *led_state = HIGH;
+        digitalWrite(LED, HIGH);
+      }
+    }
+  }
+}
+
+class MyClientCallback : public BLEClientCallbacks {
+    void onConnect(BLEClient* pclient) {
+    }
+
+    void onDisconnect(BLEClient* pclient) {
+      connected = false;
+      Serial.println("onDisconnect");
+    }
+};
+
+bool connectToServer() {
+  Serial.print("Forming a connection to ");
+  Serial.println(myDevice->getAddress().toString().c_str());
+
+  BLEClient*  pClient  = BLEDevice::createClient();
+  Serial.println(" - Created client");
+
+  pClient->setClientCallbacks(new MyClientCallback());
+
+  // Connect to the remove BLE Server.
+  pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+  Serial.println(" - Connected to server");
+  Serial.println(" - Connected to server");
+
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(serviceUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our service");
+
+
+  // Obtain a reference to the characteristic in the service of the remote BLE server.
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+  if (pRemoteCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID: ");
+    Serial.println(charUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our characteristic");
+
+  // Read the value of the characteristic.
+  if (pRemoteCharacteristic->canRead()) {
+    std::string value = pRemoteCharacteristic->readValue();
+    Serial.print("The characteristic value was: ");
+    Serial.println(value.c_str());
+  }
+
+  if (pRemoteCharacteristic->canNotify())
+    pRemoteCharacteristic->registerForNotify(notifyCallback);
+
+  connected = true;
+  return true;
+}
+/**
+   Scan for BLE servers and find the first one that advertises the service we are looking for.
+*/
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    /**
+        Called for each advertising BLE server.
+    */
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.print("BLE Advertised Device found: ");
+      Serial.println(advertisedDevice.toString().c_str());
+      //Serial.println(advertisedDevice.getAddress().toString().c_str());
+
+      if (!strcmp(advertisedDevice.getAddress().toString().c_str(), "24:6f:28:1a:d3:46")){
+        Serial.println("FOUND ESP32");
+        BLEDevice::getScan()->stop();
+        myDevice = new BLEAdvertisedDevice(advertisedDevice);
+        doConnect = true;
+        doScan = true;
+      }
+      // We have found a device, let us now see if it contains the service we are looking for.
+//      if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+//
+//        BLEDevice::getScan()->stop();
+//        myDevice = new BLEAdvertisedDevice(advertisedDevice);
+//        doConnect = true;
+//        doScan = true;
+
+//      } // Found our server
+    } // onResult
+}; // MyAdvertisedDeviceCallbacks
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting Arduino BLE Client application...");
+  BLEDevice::init("");
+
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
+  pinMode(13, OUTPUT);
+  pinMode(21, OUTPUT);
+  pinMode(12, OUTPUT);
+} // End of setup.
+
+
+// This is the Arduino main loop function.
+void loop() {
+
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+  // connected we set the connected flag to be true.
+  if (doConnect == true) {
+    if (connectToServer()) {
+      Serial.println("We are now connected to the BLE Server.");
+    } else {
+      Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+    }
+    doConnect = false;
+  }
+
+  // If we are connected to a peer BLE Server, update the characteristic each time we are reached
+  // with the current time since boot.
+  if (connected) {
+    //String newValue = "Time since boot: " + String(millis() / 1000);
+    //Serial.println("Setting new characteristic value to \"" + newValue + "\"");
+    Serial.println( pRemoteCharacteristic->readValue().c_str() );
+
+
+    // Set the characteristic's value to be the array of bytes that is actually a string.
+    //pRemoteCharacteristic->writeValue(newValue.c_str(), newValue.length());
+  } else if (doScan) {
+    BLEDevice::getScan()->start(0);  // this is just eample to start scan after disconnect, most likely there is better way to do it in arduino
+  }
+  unsigned long curr_time = millis();
+  blink(LEFT_LED, left, &left_led_state, curr_time);
+  blink(RIGHT_LED, right, &right_led_state, curr_time);
+  
+  delay(1000); // Delay a second between loops.
+} // End of loop
